73eda78ffa658a554aebc42553fc0ded07e082b1
Eric <ericoneill34@gmail.com>
2018-10-25 16:47:16 -0400

    Updates

---
+++ precache-manifest.0ad0229ce380e3feefa37c93f3197adb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb2719136a5ec057a54",
+    "url": "/lyric-app/static/js/runtime~main.4bb27191.js"
+  },
+  {
+    "revision": "59b4b29e657e0eb9896c",
+    "url": "/lyric-app/static/js/main.59b4b29e.chunk.js"
+  },
+  {
+    "revision": "cb077c9c3157bd02c83e",
+    "url": "/lyric-app/static/js/1.cb077c9c.chunk.js"
+  },
+  {
+    "revision": "59b4b29e657e0eb9896c",
+    "url": "/lyric-app/static/css/main.9743dcc1.chunk.css"
+  },
+  {
+    "revision": "694a70567c22fb9f5cd90bb9254b3ed7",
+    "url": "/lyric-app/index.html"
+  }
+];
